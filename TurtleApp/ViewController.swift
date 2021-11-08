ea03688337e20be31abd8a90a9977f10a9a98ece
Venky Venkatakrishnan <eeshwar1@gmail.com>
2021-11-08 14:02:10 -0600

    Altered Turtle Drawing function to just have 1 drawing

--- TurtleApp/ViewController.swift
+++ TurtleApp/ViewController.swift
@@ -14,28 +14,15 @@ class ViewController: NSViewController {
     fileprivate func turtleDrawing() {
         
         let turtle: Turtle = Turtle(id: "a", canvas: canvas)
-        
-        let turtle2: Turtle = Turtle(id: "b", canvas: canvas)
 
         turtle.color(color: .init(red: 255, green: 0, blue: 0, alpha: 1))
-        turtle2.color(color: .init(red: 0, green: 255, blue: 0, alpha: 1))
+       
         
-        turtle2.goto(x: 300, y: 200)
-        turtle2.forward(distance: 50)
-        turtle2.left(angle: 45)
-        turtle2.forward(distance: 100)
-        // turtle.penup()
-        turtle2.right(angle: 45)
-        turtle2.forward(distance: 150)
-        // turtle.pendown()
-        turtle2.right(angle: 60)
-        turtle2.backward(distance: 200)
-        
-        turtle.goto(x: 100, y: 100)
+        turtle.goto(x: 400, y: 150)
         
         for _ in 1...100 {
             
-            turtle.forward(distance: 200)
+            turtle.forward(distance: 600)
             turtle.left(angle: 170)
             
         }
